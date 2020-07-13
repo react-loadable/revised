f2a1872dafbd44dc7a69ecc5f0528126ff635c8a
Tran Sang <info@transang.me>
2020-07-14 07:20:19 +0900

    rendertostaticmarkup

--- example/server.js
+++ example/server.js
@@ -1,18 +1,44 @@
-import express from 'express';
-import path from 'path';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import Loadable from 'react-loadable';
-import { getBundles } from 'react-loadable-webpack'
-import App from './components/App';
+import express from 'express'
+import path from 'path'
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import Loadable from 'react-loadable'
+import {getBundles} from 'react-loadable-webpack'
+import App from './components/App'
 import fs from 'fs'
 
 const stats = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'dist/react-loadable.json'), 'utf8'));
 const app = express();
 
+const Html = ({styles, scripts, children}) => {
+  return <html lang="en">
+    <head>
+      <meta charSet="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+      <title>My App</title>
+      {
+        styles.map(style => {
+          return <link href={`/dist/${style.file}`} rel="stylesheet"/>;
+        })
+      }
+    </head>
+    <body>
+    <div id="app">{children}</div>
+    <script src="/dist/main.js"/>
+    {
+      scripts.map(script => {
+        return <script src={`/dist/${script.file}`}/>
+      })
+    }
+    <script>window.main();</script>
+    </body>
+  </html>
+}
+
 app.get('/', (req, res) => {
   let modules = [];
-  let html = ReactDOMServer.renderToString(
+  let body = ReactDOMServer.renderToString(
     <Loadable.Capture report={moduleName => modules.push(moduleName)}>
       <App/>
     </Loadable.Capture>
@@ -23,28 +49,10 @@ app.get('/', (req, res) => {
   let styles = bundles.filter(bundle => bundle.file.endsWith('.css'));
   let scripts = bundles.filter(bundle => bundle.file.endsWith('.js'));
 
-  res.send(`
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>My App</title>
-        ${styles.map(style => {
-          return `<link href="/dist/${style.file}" rel="stylesheet"/>`;
-        }).join('\n')}
-      </head>
-      <body>
-        <div id="app">${html}</div>
-        <script src="/dist/main.js"></script>
-        ${scripts.map(script => {
-          return `<script src="/dist/${script.file}"></script>`
-        }).join('\n')}
-        <script>window.main();</script>
-      </body>
-    </html>
-  `);
+  res.send(`<!doctype html>
+${ReactDOMServer.renderToStaticMarkup(<Html styles={styles} scripts={scripts}>
+    {body}
+  </Html>)}`)
 });
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
